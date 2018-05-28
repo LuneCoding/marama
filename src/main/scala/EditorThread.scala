3ebd8221d778aad1d3b2394cacf311ba89b88060
jjkramok <jjkramok@gmail.com>
2018-05-28 10:15:05 +0200

    Implemented basic com. structure.

--- src/main/scala/EditorThread.scala
+++ src/main/scala/EditorThread.scala
@@ -1,7 +1,32 @@
 package editor
 
+import java.util.concurrent.{ArrayBlockingQueue, BlockingQueue}
+
 class EditorThread extends Runnable {
+  var requests : BlockingQueue[String] = new ArrayBlockingQueue[String](5)
+  var answers : BlockingQueue[String] = new ArrayBlockingQueue[String](5)
+
   def run: Unit = {
-    println("EditorThread started!")
+    println("Editor: Thread started!")
+    var msg : String = ""
+
+    while (msg.equals("quit") || msg.equals("stop")) {
+      Thread.sleep(10)
+      if (msg == null || msg.isEmpty) {
+        msg = requests.take()
+      }
+
+      // Echo and print received message for clarification.
+      if (msg != null) {
+        println("Editor: Received request - {0}", msg)
+        answers.add(msg)
+      }
+
+      // other message handling logic goes here
+
+      // Clear the msg so a new one can be received next iteration.
+      msg = ""
+    }
+    println("Editor: Goodbye.")
   }
 }
